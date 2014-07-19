be19ab0eef5c86d668119ad11b73f739b6f8af5d
Marco Au <marco.au.321@gmail.com>
2014-07-18 18:32:22 -0700

    Converted Courier functions to async

--- courier.js
+++ courier.js
@@ -1,6 +1,6 @@
-var _ = require('underscore');
+var http = require('http');
 
-var httpSync = require('http-sync'); // hack: use synchronous http requests for the tests
+var _ = require('underscore');
 var parseString = require('xml2js').parseString; // use XML to JS object parser (for USPS)
 var cheerio = require('cheerio'); // HTML string to DOM object parser (for HKP)
 
@@ -10,8 +10,8 @@ var formatTime = function(date){
   return moment(date).format('YYYY-MM-DDThh:mm:ss');
 };
 
-function Courier() {
-  this.usps = function(tracking_number) {
+var Courier = function() {
+  this.usps = function(tracking_number, callback) {
     var tracking_result = {};
 
     // options for USPS http request
@@ -21,34 +21,46 @@ function Courier() {
         tracking_number + '%22%3E%3C/TrackID%3E%3C/TrackFieldRequest%3E'
     };
 
-    // synchronous (blocking) http request
-    var request = httpSync.request(options);
-    var response = request.end();
-
-    // parse the response XML body; function is synchronous
-    parseString(response.body, function(err, result){
-      var trackSummary = result.TrackResponse.TrackInfo[0].TrackSummary[0];
-      // can manipulate the array result.TrackResponse.TrackInfo[0].TrackDetail if more details are needed
-
-      // extract info from trackSummary
-      var countryName = trackSummary.EventCountry[0];
-      var message = trackSummary.Event[0];
-      var checkpointTime = formatTime(trackSummary.EventDate[0] + ' ' + trackSummary.EventTime[0]);
-
-      //fixit: need to accommodate multiple events
-      tracking_result.checkpoints = [];
-      tracking_result.checkpoints.push({
-        country_name: countryName,
-        message: message,
-        checkpoint_time: checkpointTime
+    http.get(options, function(res) {
+      var data = '';
+      res.on('data', function(chunk){
+        data += chunk;
       });
-
+      res.on('end', function(){
+        processData(data);
+      });
+    }).on('error', function(err) {
+      console.error(err);
     });
 
-    return tracking_result;
+    var processData = function(data){
+      // parse the response XML body; function is synchronous
+      parseString(data, function(err, result){
+        console.log(result);
+        var trackSummary = result.TrackResponse.TrackInfo[0].TrackSummary[0];
+        // can manipulate the array result.TrackResponse.TrackInfo[0].TrackDetail if more details are needed
+
+        // extract info from trackSummary
+        var countryName = trackSummary.EventCountry[0];
+        var message = trackSummary.Event[0];
+        var checkpointTime = formatTime(trackSummary.EventDate[0] + ' ' + trackSummary.EventTime[0]);
+
+        //fixit: need to accommodate multiple events
+        tracking_result.checkpoints = [];
+        tracking_result.checkpoints.push({
+          country_name: countryName,
+          message: message,
+          checkpoint_time: checkpointTime
+        });
+
+        callback(tracking_result);
+      });
+
+    };
+
   };
 
-  this.hkpost = function(tracking_number) {
+  this.hkpost = function(tracking_number, callback) {
     var tracking_result = {};
 
     // options for HKP http request
@@ -57,45 +69,55 @@ function Courier() {
       path: '/CGI/mt/mtZresult.jsp?tracknbr=' + tracking_number
     };
 
-    // synchronous (blocking) http request
-    var request = httpSync.request(options);
-    var response = request.end();
-
-    // use cheerio to convert response body to 'DOM object'
-    var $ = cheerio.load(String(response.body));
-
-    // get text from #clfContent div and split into array
-    var htmlData = _.filter($('#clfContent').text().split('  '), function(el){
-      return el.length;
-    });
-    // purge data (cleared empty spaces)
-    var data = htmlData[3].split('\r\n');
-    
-    // get country name from data
-    var countryNameData = data[data.length - 2].split(' ');
-    var countryName = countryNameData[countryNameData.length - 1];
-
-    // slice out appropriate message from data
-    var messageData = data[data.length - 1];
-    // hack: use trick to slice off tracking number from message
-    var numberSlicePos = messageData.indexOf(tracking_number);
-    var message = messageData.slice(0, numberSlicePos - 1) +
-      messageData.slice(numberSlicePos + tracking_number.length + 2, messageData.length - 1);
-
-    // hack: get checkpoint time from message
-    var checkpointTime = formatTime(message.slice(message.length - 12, message.length - 1));
-
-    tracking_result.checkpoints = [];
-    tracking_result.checkpoints.push({
-      country_name: countryName,
-      message: message,
-      checkpoint_time: checkpointTime
+    http.get(options, function(res) {
+      var data = '';
+      res.on('data', function(chunk){
+        data += chunk;
+      });
+      res.on('end', function(){
+        processData(data);
+      });
+    }).on('error', function(err) {
+      console.error(err);
     });
 
-    return tracking_result;
+    var processData = function(data){
+      // use cheerio to convert response body to 'DOM object'
+      var $ = cheerio.load(String(data));
+
+      // get text from #clfContent div and split into array
+      var htmlData = _.filter($('#clfContent').text().split('  '), function(el){
+        return el.length;
+      });
+      // purge data (cleared empty spaces)
+      var data = htmlData[3].split('\r\n');
+      
+      // get country name from data
+      var countryNameData = data[data.length - 2].split(' ');
+      var countryName = countryNameData[countryNameData.length - 1];
+
+      // slice out appropriate message from data
+      var messageData = data[data.length - 1];
+      // hack: use trick to slice off tracking number from message
+      var numberSlicePos = messageData.indexOf(tracking_number);
+      var message = messageData.slice(0, numberSlicePos - 1) +
+        messageData.slice(numberSlicePos + tracking_number.length + 2, messageData.length - 1);
+
+      // hack: get checkpoint time from message
+      var checkpointTime = formatTime(message.slice(message.length - 12, message.length - 1));
+
+      tracking_result.checkpoints = [];
+      tracking_result.checkpoints.push({
+        country_name: countryName,
+        message: message,
+        checkpoint_time: checkpointTime
+      });
+
+      callback(tracking_result);
+    };
   };
 
-  this.dpduk = function(tracking_number) {
+  this.dpduk = function(tracking_number, callback) {
     var tracking_result = {};
 
     // options for DPD UK http request
@@ -108,40 +130,50 @@ function Courier() {
       }
     };
 
-    // synchronous (blocking) http request
-    var request = httpSync.request(options);
-    var response = request.end();
-
-    // get JSON data from response
-    var jsonData = JSON.parse(response.body).obj;
-    var trackingEvents = jsonData.trackingEvent;
-
-    // format the events data and push into tracking_result.checkpoints
-    tracking_result.checkpoints = [];
-    _.each(trackingEvents, function(event){
-
-      // get data for each event
-      var countryName = event.trackingEventLocation;
-      var message = event.trackingEventStatus;
-      // hack: trick to slice out time zone to make test pass
-      var checkpointTime = event.trackingEventDate.slice(0, event.trackingEventDate.length - 5);
-
-      tracking_result.checkpoints.push({
-        country_name: countryName,
-        message: message,
-        checkpoint_time: checkpointTime
+    http.get(options, function(res) {
+      var data = '';
+      res.on('data', function(chunk){
+        data += chunk;
       });
-    })
-    
-    //reverse checkpoints array to make it oldest first
-    tracking_result.checkpoints.reverse();
+      res.on('end', function(){
+        processData(data);
+      });
+    }).on('error', function(err) {
+      console.error(err);
+    });
 
-    return tracking_result;
+    // // synchronous (blocking) http request
+    // var request = httpSync.request(options);
+    // var response = request.end();
+
+    var processData = function(data){
+      // get JSON object from response data
+      var jsonData = JSON.parse(data).obj;
+      var trackingEvents = jsonData.trackingEvent;
+
+      // format the events data and push into tracking_result.checkpoints
+      tracking_result.checkpoints = [];
+      _.each(trackingEvents, function(event){
+
+        // get data for each event
+        var countryName = event.trackingEventLocation;
+        var message = event.trackingEventStatus;
+        // hack: trick to slice out time zone to make test pass
+        var checkpointTime = event.trackingEventDate.slice(0, event.trackingEventDate.length - 5);
+
+        tracking_result.checkpoints.push({
+          country_name: countryName,
+          message: message,
+          checkpoint_time: checkpointTime
+        });
+      })
+      
+      //reverse checkpoints array to make it oldest first
+      tracking_result.checkpoints.reverse();
+
+      callback(tracking_result);
+    };
   };
 }
 
 module.exports = new Courier();
-
-console.log(new Courier().usps('9405903699300184125060'));
-console.log(new Courier().hkpost('RC933607107HK'));
-console.log(new Courier().dpduk('15502370264989N'));
